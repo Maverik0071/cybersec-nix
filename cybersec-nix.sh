e968e2fbe41592d2834b3cf7bc48ea6ce43a30fc
Maverik0071 <55121581+Maverik0071@users.noreply.github.com>
2025-08-22 16:51:46 -0400

    Add files via upload

---
+++ cybersec-nix.sh
@@ -0,0 +1,16 @@
+# script for installing the curl command with my nixos commands
+
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable
+sudo nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+
+git clone https://www.github.com/Maverik0071/nixos-config.git
+
+sudo cp ~/nixos-config/modules/cybersec-config.nix /etc/nixos/configuration.nix
+
+cd /etc/nixos/
+
+sudo flake nix init
+
+sudo cp ~nixos-config/flakes/flake.nix /etc/nixos/flake.nix
+
+sudo nixos-rebuild switch
